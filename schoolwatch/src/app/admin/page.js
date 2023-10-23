1568781cb67c1718b295b68435ae54f1aadd527b
leonyarov <leon44ml@gmail.com>
2023-10-23 13:43:54 +0300

    initial commit

---
+++ schoolwatch/src/app/admin/page.js
@@ -0,0 +1,5 @@
+export default function Page() {
+    return <div> <h1>Hello, This is a dashboard</h1>
+                <a href="/">Go Back</a>
+            </div>
+  }
---
+++ schoolwatch/src/app/page.js
@@ -0,0 +1,6 @@
+export default function Page() {
+    return <div>
+                <h1>Hello, Next.js!</h1>
+                <a href="/admin">Go to Admin</a>
+        </div>
+  }
